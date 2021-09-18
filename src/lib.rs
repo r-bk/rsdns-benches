93deaac0be81bd8904b2fcb7d566a834a05e243f
Rafael Buchbinder <rafi@rbk.io>
2021-09-18 11:07:29 +0300

    lib: fix rsdns link

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! `rsdns-benches` is a dummy cargo project dedicated to [`rsdns`] benchmarking.
+//!
+//! [`rsdns`]: https://docs.rs/rsdns
 
+/// Dummy function.
 pub fn hello() {
     println!("rsdns-benches: benchmarking rsdns");
 }
